1c174b103fef45a4da9fc52d9349d31c2822be52
Nicole Bills <bnicolebills@gmail.com>
2019-11-19 11:48:47 -0500

    update data_cleaning file

--- python_files/data_cleaning.py
+++ python_files/data_cleaning.py
@@ -10,19 +10,21 @@ Each support function should have an informative name and return the partially c
 
 import pandas as pd
 
-def clean_eviction_data(e_df, year, GEOIDs_list):
+def clean_eviction_data(df, year, GEOIDs_list):
     """Limits dataset to a given year and list of GEOIDs"""
     df = df.loc[df.year == year]
     df = df.loc[df['GEOID'].isin(GEOIDs_list)]
     df.reset_index(inplace=True)
     return df
 
-def merge_in_puds(e_df,p_df):
+def merge_in_puds(df,p_df):
     """Merges main dataset with PUD dataset based on crosswalk"""
     crosswalk = pd.read_csv('../data/ward8PUDcrosswalk.csv')
-    df = p_df.merge(crosswalk[['OBJECTID','GEOID']], on='OBJECTID')
-    e_df = e_df.merge(p_df, on='GEOID')
-    return e_df
+    p_df = p_df.merge(crosswalk[['OBJECTID','GEOID']], on='OBJECTID')
+    df = df.merge(p_df, on='GEOID', how='outer')
+    return df
+
+ward8 = [11001009801,11001009802,11001009803,11001009804,11001007502,11001007503,11001007504,11001009807,11001009810,11001009811,11001009700,11001007401,11001007403,11001007404,11001007406,11001007407,11001007408,11001007409,11001007301,11001007304,11001010900,11001010400,11001007601]
 
 def full_clean():
     """
@@ -33,9 +35,10 @@ def full_clean():
     """
     evict = pd.read_csv("../data/EvictionLab/tracts.csv")
     puds = pd.read_csv("../data/OpenData/Planned_Unit_Development_PUDs.csv")
-
-    evict = support_function_one(evict)
-    evict = support_function_two(evict,puds)
-    evict.to_csv('./data/cleaned_for_testing.csv')
+    
+    evict = clean_eviction_data(evict, 2016, ward8)
+    evict = merge_in_puds(evict,puds)
+    evict.drop(['index'],axis=1, inplace=True)
+    evict.to_csv('../data/cleaned_for_testing.csv', index=False)
     
     return evict
