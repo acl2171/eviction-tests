71e612e5ab4a29e77e5098e85dba659e5678681f
Nicole Bills <bnicolebills@gmail.com>
2019-11-19 10:24:02 -0500

    add data cleanup and image files

--- python_files/data_cleaning.py
+++ python_files/data_cleaning.py
@@ -7,16 +7,22 @@ This module is for data cleaning.
 There can be an unlimited amount of support functions.
 Each support function should have an informative name and return the partially cleaned bit of the dataset.
 """
+
 import pandas as pd
 
-def support_function_one(example):
-    pass
+def clean_eviction_data(e_df, year, GEOIDs_list):
+    """Limits dataset to a given year and list of GEOIDs"""
+    df = df.loc[df.year == year]
+    df = df.loc[df['GEOID'].isin(GEOIDs_list)]
+    df.reset_index(inplace=True)
+    return df
 
-def support_function_two(example):
-    pass
-
-def support_function_three(example):
-    pass
+def merge_in_puds(e_df,p_df):
+    """Merges main dataset with PUD dataset based on crosswalk"""
+    crosswalk = pd.read_csv('../data/ward8PUDcrosswalk.csv')
+    df = p_df.merge(crosswalk[['OBJECTID','GEOID']], on='OBJECTID')
+    e_df = e_df.merge(p_df, on='GEOID')
+    return e_df
 
 def full_clean():
     """
@@ -25,11 +31,11 @@ def full_clean():
 
     :return: cleaned dataset to be passed to hypothesis testing and visualization modules.
     """
-    dirty_data = pd.read_csv("./data/dirty_data.csv")
+    evict = pd.read_csv("../data/EvictionLab/tracts.csv")
+    puds = pd.read_csv("../data/OpenData/Planned_Unit_Development_PUDs.csv")
 
-    cleaning_data1 = support_function_one(dirty_data)
-    cleaning_data2 = support_function_two(cleaning_data1)
-    cleaned_data= support_function_three(cleaning_data2)
-    cleaned_data.to_csv('./data/cleaned_for_testing.csv')
+    evict = support_function_one(evict)
+    evict = support_function_two(evict,puds)
+    evict.to_csv('./data/cleaned_for_testing.csv')
     
-    return cleaned_data
+    return evict
